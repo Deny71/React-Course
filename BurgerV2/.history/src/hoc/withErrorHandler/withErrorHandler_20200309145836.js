a3a954ea2a0658ebcb9374c95fda08380519c3a9
Pecko <damian.pecko@accenture.com>
2020-03-10 13:50:42 +0100

    Burger+Firebird

---
+++ BurgerV2/.history/src/hoc/withErrorHandler/withErrorHandler_20200309145836.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal'
+import Auxiliary from '../Auxiliary/Auxiliary'
+
+const withErrorHandler = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <Auxiliary>
+                <Modal>
+                    Something didnt work!
+                </Modal>
+            <WrappedComponent {...props}/>
+            </Auxiliary>
+        );
+    }
+}
+
+export default withErrorHandler;
